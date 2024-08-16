c99d46b83d5c9a1a588d37d393978ecc41e4c3cf
novelyear <3507097023@qq.com>
2024-08-16 16:28:25 +0800

    增加自动填充过程日志

--- sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
+++ sky-server/src/main/java/com/sky/aspect/AutoFillAspect.java
@@ -19,51 +19,57 @@ import java.time.LocalDateTime;
 @Aspect
 @Slf4j
 public class AutoFillAspect {
+    /**
+     * 切入点
+     */
     @Pointcut("execution(* com.sky.mapper.*.*(..)) && @annotation(com.sky.annotation.AutoFill)")
     public void autoFillPointCut(){}
+
     @Before("autoFillPointCut()")
     public void autoFill(JoinPoint joinPoint){
         log.info("自动填充");
-        //获取operationType
+        //获取数据库操作类型：create or update ?
         MethodSignature signature = (MethodSignature)joinPoint.getSignature();
         AutoFill autoFill = signature.getMethod().getAnnotation(AutoFill.class);
         OperationType operationType = autoFill.value();
-
+        //获取被拦截方法的参数，以写入信息
         Object[] args = joinPoint.getArgs();
-
+        if(args == null || args.length == 0){
+            return;
+        }
+        //约定实体对象放在第一位
         Object entity = args[0];
-
+        //要赋值的数据
         LocalDateTime now = LocalDateTime.now();
         Long currentId = BaseContext.getCurrentId();
-
+        log.info("操作类型为{}", operationType);
         if(operationType == OperationType.INSERT){
             try {
                 Method setCreateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_TIME, LocalDateTime.class);
                 Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
                 Method setCreateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_CREATE_USER, Long.class);
                 Method setUpdateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
-
+                //通过反射为对象属性赋值
                 setCreateTime.invoke(entity, now);
                 setUpdateTime.invoke(entity, now);
                 setCreateUser.invoke(entity, currentId);
                 setUpdateUser.invoke(entity, currentId);
+                log.info("Entity HashCode after modification: {}", System.identityHashCode(entity));
+                log.info("已填充初始及修改信息");
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
-
-        }else {
+        }else if(operationType == OperationType.UPDATE){
             try {
                 Method setUpdateTime = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_TIME, LocalDateTime.class);
                 Method setUpdateUser = entity.getClass().getDeclaredMethod(AutoFillConstant.SET_UPDATE_USER, Long.class);
 
                 setUpdateTime.invoke(entity, now);
                 setUpdateUser.invoke(entity, currentId);
+                log.info("已填充修改信息");
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
-
-
-
     }
 }
